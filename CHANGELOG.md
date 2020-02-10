fcd0d881c53c21bf2aeada5030427ce30a25ebbc
Fridolín Pokorný <fpokorny@redhat.com>
2020-02-10 12:54:38 +0100

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog for Thoth's Template GitHub Project
+
+## [0.1.0] - 2019-Sep-11 - goern
+
+### Added
+
+all the things that you see...
---
+++ app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# project template
+# Copyright(C) 2010 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and / or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""This is the main script of the template project."""
+
+from template.version import __version__
+
+if __name__ == "__main__":
+    print(f"A template project with Thoth integration, v{__version__}.")
