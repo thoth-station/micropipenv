84dbfb2f0a1abf57cbbf9efa386c980cca604f7c
Fridolin Pokorny <fridolin.pokorny@gmail.com>
2020-03-12 19:33:04 +0100

    Version 0.1.4

--- micropipenv.py
+++ micropipenv.py
@@ -26,7 +26,7 @@ dependencies using raw pip. The virtual environment is not created, but one can
 issue `python3 -m venv venv/ && . venv/bin/activate` to create one.
 """
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 __author__ = "Fridolin Pokorny <fridex.devel@gmail.com>"
 __title__ = "micropipenv"
 __all__ = [
