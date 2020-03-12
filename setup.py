a3d8212213df1e39fa33fa753272069a18c1e388
Fridolin Pokorny <fridolin.pokorny@gmail.com>
2020-03-12 13:09:21 +0100

    Add LICENSE file to setup.py

--- setup.py
+++ setup.py
@@ -66,6 +66,7 @@ setup(
     author_email="fridex.devel@gmail.com",
     license="GPLv3+",
     py_modules=["micropipenv"],
+    data_files=[("", ["LICENSE"])],
     cmdclass={"test": Test},
     entry_points={"console_scripts": ["micropipenv=micropipenv:main"]},
     extras_require={
