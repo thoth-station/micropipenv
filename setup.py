dcd33dbb0c6866aa7d9405f94d6a9fded61d35c8
Lumir Balhar <lbalhar@redhat.com>
2020-03-19 11:23:18 +0100

    Implement tox to test micropipenv with all supported Pythons

--- setup.py
+++ setup.py
@@ -16,45 +16,6 @@ def get_version():
 
     raise ValueError("No version identifier found")
 
-
-class Test(TestCommand):
-    """Introduce test command to run testsuite using pytest."""
-
-    _IMPLICIT_PYTEST_ARGS = [
-        "--timeout=300",
-        "--mypy",
-        "micropipenv.py",
-        "--capture=no",
-        "--verbose",
-        "-l",
-        "-s",
-        "-vv",
-        "tests/",
-    ]
-
-    user_options = [("pytest-args=", "a", "Arguments to pass into py.test")]
-
-    def initialize_options(self):
-        super().initialize_options()
-        self.pytest_args = None
-
-    def finalize_options(self):
-        super().finalize_options()
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import pytest
-
-        passed_args = list(self._IMPLICIT_PYTEST_ARGS)
-
-        if self.pytest_args:
-            self.pytest_args = [arg for arg in self.pytest_args.split() if arg]
-            passed_args.extend(self.pytest_args)
-
-        sys.exit(pytest.main(passed_args))
-
-
 setup(
     name="micropipenv",
     version=get_version(),
@@ -66,7 +27,6 @@ setup(
     author_email="fridex.devel@gmail.com",
     license="GPLv3+",
     py_modules=["micropipenv"],
-    cmdclass={"test": Test},
     install_requires=["pip"],
     entry_points={"console_scripts": ["micropipenv=micropipenv:main"]},
     extras_require={
