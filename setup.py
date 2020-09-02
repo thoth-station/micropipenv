8710d6c00be2f7b2b91fd6a02d230f594fde912e
Fridolin Pokorny <fridolin.pokorny@gmail.com>
2020-09-02 13:01:06 +0200

    Drop Python 3.5 support

--- setup.py
+++ setup.py
@@ -39,7 +39,6 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
